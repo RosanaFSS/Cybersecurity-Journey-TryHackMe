5042824342756b795b6764cc90d78ef2db52e5cd
Rosana <53669772+RosanaFSS@users.noreply.github.com>
2025-04-09 17:35:59 -0300

    [OK]

--- Easy 🔗 - Breaking Crypto the Simple Way - Task 3.py
+++
@@ -1,3 +0,0 @@
-AWS S B - 2023, August 18, AWS Partner - Migration Essentials (Technical)
-AWS S B - 2023, August 16, AWS Partner - AWS Cloud Practitioner Essentials
-AWS S B - 2023, August 09, AWS Partner - Security Governance at Scale (Technical)/º§
